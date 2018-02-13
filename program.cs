3a500698a0067caae09b5cccbeb73c18af671842
Edward R <96d7cf74@opayq.com>
2018-02-13 16:43:11 -0500

    Converted string to integer

--- program.cs
+++ program.cs
@@ -6,16 +6,22 @@ namespace SoftwareInc
     {
         static void Main()
         {
+
+            int runningTotal = 0;
+
             // Prompt the user for minutes exercised
             Console.Write("How many minutes did you exercise?  ");
+
             string entry = Console.ReadLine();
 
+            int minutes = int.Parse(entry);   // Change string to integer
+            runningTotal = runningTotal + minutes;
 
             // Add minutes exercised to total
 
 
             // Display total minutes exercised to the screen
-                Console.WriteLine("You entered " + entry + " minutes");
+                Console.WriteLine("You entered " + runningTotal + " minutes");
 
             // repeat until the user quits
 
