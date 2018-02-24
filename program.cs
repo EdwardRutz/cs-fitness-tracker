0ab2f1dedd3029a7f1066d6e6bb2697d843c41b9
EdwardRutz <96d7cf74@opayq.com>
2018-02-24 16:45:51 -0500

    Added Messages - Added positvie reinforcement messages for time spent exercising

--- program.cs
+++ program.cs
@@ -26,6 +26,23 @@ namespace Treehouse.FitnessFrog
                 {
                     int minutes = int.Parse(entry);
 
+                    if(minutes <= 10)
+                    {
+                        Console.WriteLine("Your off to a good start!");
+                    }
+                    else if (minutes <= 30)
+                    {
+                        Console.WriteLine("Way to go, you are smoke'n!");
+                    }
+                    else if(minutes <= 60)
+                    {
+                        Console.WriteLine("You're a Ninja Warrior in Training!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("You are rock'n the house!");
+                    }
+
                     // Add minutes exercised to total
                     runningTotal = runningTotal + minutes;
 
