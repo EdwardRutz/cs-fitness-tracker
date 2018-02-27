bd925cb936d43a15b6cd30d572c98cedf5221c68
EdwardRutz <96d7cf74@opayq.com>
2018-02-26 21:36:33 -0500

    Input Validation - Add try/catch exception handling for strings entered as numbers - added VS2017 project to source files

--- program.cs
+++ program.cs
@@ -7,32 +7,34 @@ namespace Treehouse.FitnessFrog
         static void Main()
         {
 
-            int runningTotal = 0;
-            bool keepGoing = true;
+            double runningTotal = 0.0;
 
             //To stop the loop, just set keepGoing to false when the stop condition is met
 
-            while (keepGoing)
+            while (true)
             {
                 // Prompt the user for minutes exercised
                 Console.Write("How many minutes did you exercise or type \"quit\" to exit: ");
-                string entry = Console.ReadLine();
+                var entry = Console.ReadLine();
 
-                if (entry == "quit")
+                if (entry.ToLower() == "quit")  //toLower() makes entry all lower case
                 {
-                    keepGoing = false;
+                    break;
                 }
-                else
+                
+                //Use try-catch to handle exceptions
+                try
                 {
-                    int minutes = int.Parse(entry);
+                    //make sure everything that uses the variable minutes is within curly braces
 
-                    if(minutes <= 0)
+                    var minutes = double.Parse(entry);
+
+                    if (minutes <= 0)
                     {
                         Console.WriteLine(minutes + " is not an acceptable value");
                         continue;  //continue tells the loop to start from the beginning
                     }
-
-                    else if(minutes <= 10)
+                    else if (minutes <= 10)
                     {
                         Console.WriteLine("Your off to a good start!");
                     }
@@ -40,7 +42,7 @@ namespace Treehouse.FitnessFrog
                     {
                         Console.WriteLine("Way to go, you are smoke'n!");
                     }
-                    else if(minutes <= 60)
+                    else if (minutes <= 60)
                     {
                         Console.WriteLine("You're a Ninja Warrior in Training!");
                     }
@@ -50,11 +52,16 @@ namespace Treehouse.FitnessFrog
                     }
 
                     // Add minutes exercised to total
-                    runningTotal = runningTotal + minutes;
-
+                    runningTotal = runningTotal + minutes;      //runningTotal = runningTotal + minutes;
+                }
+                    catch(FormatException)
+                    {
+                        Console.WriteLine("That is not valid input.");
+                        continue;
+                    }                  
                     // Display total minutes exercised to the screen
                     Console.WriteLine("You entered " + runningTotal + " minutes");
-                }
+                    
                 // repeat until the user quits
             }
         }
